835be9164f625c5295f94718154c594ec288f2f7
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-06-05 12:30:32 +0530

    Valid first name

---
+++ UserRegistrationMain.java
@@ -0,0 +1,27 @@
+package Com.Bridgelabz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationMain {
+    public void firstName() {
+
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+        if (result == true)
+            System.out.println("First Name is valid");
+        else
+            System.out.println("First Name is Invalid");
+    }
+
+    public static void main(String[] args) {
+        UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
+        userRegistrationMain.firstName();
+    }
+}
